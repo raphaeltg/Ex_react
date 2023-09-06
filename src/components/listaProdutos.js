b74032f54b49bc25349c8c91cd9988690b3001bc
raphaeltg <rm99354@fiap.com.br>
2023-09-06 03:37:56 -0300

    Finalização

---
+++ src/components/listaProdutos.js
@@ -0,0 +1,19 @@
+export const listaProdutos = [
+    {
+
+        id: 1,
+        nome: 'Mouse',
+        preco: 70
+    },
+    {
+
+        id: 2,
+        nome: 'Teclado',
+        preco: 150
+    },
+    {
+        id: 3,
+        nome: 'Monitor',
+        preco: 600
+    },
+]
--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../../components/ListaProdutos"
+
+
+
+
 export default function Produtos(){
 
     return(
 
         <main>
             <h1>PRODUTOS</h1>
+            {listaProdutos.map(prod => (
+                <div key ={prod.id}>
+                    <Link to={'/produtos/editar/${prod.id'}>
+                        Editar o produto: {prod.name}
+                    </Link>
+
+                </div>
+            ))}
         </main>
         
     )
