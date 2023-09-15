7fb02c9920cc3cd7205836b6954f5252bcd616f5
raphaeltg <rm99354@fiap.com.br>
2023-09-15 02:44:24 -0300

    Redirecionando Endereço

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
@@ -30,6 +30,10 @@ const router = createBrowserRouter([
         path: "/produtos/editar/:id",
         element: <EditarProdutos/>
       },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
+      }
     ]
   }
 
